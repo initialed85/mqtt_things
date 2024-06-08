d1267b9b17d8cf5025ca426abe41ada830949aee
Edward Beech <initialed85@gmail.com>
2024-06-08 15:45:55 +0800

    WIP

--- cmd/scratch_cli/cmd.go
+++ cmd/scratch_cli/cmd.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"time"
+
+	"github.com/initialed85/mqtt_things/pkg/broadlink_client"
+)
+
+func main() {
+	c, err := broadlink_client.NewClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ds, err := c.Discover(time.Second * 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, d := range ds {
+		log.Printf("%v - %v - %v", d.Name, d.MAC.String(), d.Addr.IP)
+	}
+}
